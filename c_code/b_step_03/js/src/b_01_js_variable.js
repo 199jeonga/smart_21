c8afd6853074ac7ed395f3f0c6be78d940ac21c5
jeonga <lja3248@gmail.com>
2021-11-03 22:44:54 +0900

    add code flex and js variable

---
+++ c_code/b_step_03/js/src/b_01_js_variable.js
@@ -0,0 +1,4 @@
+ // b_01_js_vriable.js
+
+var num = 0;
+var n;
---
+++ c_code/b_step_03/js/src/b_01_writes.js
@@ -0,0 +1,17 @@
+// 한줄 주석
+
+/* 
+여러줄 주석
+*/
+
+document.write('브라우저에 글을 입력하여 결과를 확인하는 기능 <br />');
+// alert('결과를 확인하는 기능');
+
+var who = prompt('이름을 입력해 주세요.');
+alert(who + '님 어서요세요.');
+
+console.log('콘솔창에 글자가 나타납니다.')
+console.info('log와 보여주는 형태는 같음. 정보의 표현이라는 뜻')
+console.warn('브라우저 사용에는 문제가 없지만 약간의 경고의 뜻')
+console.error('개발 시 또는 사용시 문제가 되는 부분을 강제로 에러 표시하여 처리')
+console.clear();
