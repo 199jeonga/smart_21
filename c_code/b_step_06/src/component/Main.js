57836ef18f7b6d573e5428a03c5b3714c1844fa0
jeonga lee <lja3248@gmail.com>
2022-01-05 22:28:33 +0900

    [feat] react : input and useState linked

--- c_code/b_step_06/src/component/Main.js
+++ c_code/b_step_06/src/component/Main.js
@@ -1,13 +1,12 @@
 import ConBox from './ConBox';
-import Review from './Review';
+import Review from './Review.jsx';
 
 export default function Main() {
   return (
     <main className="container">
         <section id="viewBox">section</section>
         <ConBox />
-        <Review />
-        
+        <Review />        
     </main>
   )
 }
